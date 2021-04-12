08f2ad03086e7630695b5412c70eae0355392c08
Pete Savage <psavage@redhat.com>
2021-04-12 13:35:50 +0100

    Updating image to have metrics

--- clowder-hello.go
+++ clowder-hello.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	client "github.com/redhatinsights/app-common-go/pkg/api/v1"
 )
 
@@ -17,16 +18,44 @@ func main() {
 	if len(os.Args) > 1 {
 		fmt.Printf("Hi")
 	} else {
-		port := client.LoadedConfig.WebPort
-		http.HandleFunc("/", helloHandler)
-		http.HandleFunc("/healthz", helloHandler)
+		mux := http.NewServeMux()
+		port := *client.LoadedConfig.PublicPort
+		mux.HandleFunc("/", helloHandler)
+		mux.HandleFunc("/healthz", helloHandler)
 
 		address := fmt.Sprintf(":%d", port)
 
-		fmt.Printf("Started, serving at %s\n", address)
-		err := http.ListenAndServe(address, nil)
-		if err != nil {
-			panic("ListenAndServe: " + err.Error())
+		server := http.Server{
+			Addr:    address,
+			Handler: mux,
 		}
+
+		outputChannel := make(chan error)
+
+		go func() {
+			fmt.Printf("Started serving base service at %s\n", address)
+			err := server.ListenAndServe()
+			if err != nil {
+				outputChannel <- err
+			}
+		}()
+
+		mux2 := http.NewServeMux()
+		mux2.Handle(client.LoadedConfig.MetricsPath, promhttp.Handler())
+
+		server2 := http.Server{
+			Addr:    fmt.Sprintf(":%d", client.LoadedConfig.MetricsPort),
+			Handler: mux2,
+		}
+		go func() {
+			fmt.Printf("Started serving metrics service at :%d%s\n", client.LoadedConfig.MetricsPort, client.LoadedConfig.MetricsPath)
+			err := server2.ListenAndServe()
+			if err != nil {
+				outputChannel <- err
+			}
+		}()
+
+		err := <-outputChannel
+		fmt.Printf("got err %s", err.Error())
 	}
 }
--- go.mod
+++ go.mod
@@ -3,5 +3,9 @@ module github.com/redhatinsights/clowder-hello
 go 1.14
 
 require (
-	github.com/redhatinsights/app-common-go v0.0.0-20201103171931-3c874ef92d2a
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/prometheus/client_golang v1.10.0
+	github.com/prometheus/common v0.20.0 // indirect
+	github.com/redhatinsights/app-common-go v1.3.0
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
 )
